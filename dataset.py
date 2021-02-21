496ca6fd2466239c5a941537b0ec8c5babb55e32
clems grisi <clementnet@outlook.com>
2021-02-21 21:34:04 +0100

    added datamodule

--- dataset.py
+++ dataset.py
@@ -28,19 +28,18 @@ class OneChannelDataset(torch.utils.data.Dataset):
 
   def __init__(self, df, signal_id=0, signal_freq=100):
 
-    self.dset = df
-    self.targets = pd.read_csv(csv_path)
+    self.df = df
     self.signal_id = signal_id
     self.freq = signal_freq
   
   def __len__(self):
-    return len(self.dset)
+    return len(self.df)
   
   def __getitem__(self, idx):
     
-    sample_index = self.dset[idx, 0]
-    subject_index = self.dset[idx, 1]
-    x = self.dset[idx, 2+9000*self.signal_id:2+9000*(self.signal_id+1)]
+    sample_index = self.df[idx, 0]
+    subject_index = self.df[idx, 1]
+    x = self.df[idx, 2+9000*self.signal_id:2+9000*(self.signal_id+1)]
     x = x.reshape(-1, self.freq)
     y = self.targets[self.targets['ID'] == sample_index].values[0][1:]
 
