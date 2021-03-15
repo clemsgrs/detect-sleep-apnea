e009ae368a20fc0bbde215a7be17c9a92442be36
clems grisi <clementnet@outlook.com>
2021-03-15 11:37:48 +0100

    added ens challenge h5 formatted data download links

---
+++ dosed/h5data_download.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+gdown --id 1V5ixY74WfO1mqmdjlo6asebQ19kwjC8R
+gdown --id 1-1l6uLuMGLgaold-OjwOiOFVrVfNmahI
+unzip -q h5_train.zip
+rm h5_train.zip
+unzip -q h5_test.zip
+rm h5_test.zip
